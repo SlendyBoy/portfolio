c57f6326fb32c259516e8355c82522db5d960c5d
Tanguy Frouin <tanguy.frouin@etu.univ-lyon1.fr>
2021-01-15 01:03:13 +0100

    import portfolio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/Resume";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as HashRouter, Route, Switch } from "react-router-dom";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ScrollToTop from "./components/ScrollToTop";
@@ -19,7 +19,7 @@ function App() {
     }, 1200);
   }, []);
   return (
-    <Router>
+    <HashRouter basename="/">
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
@@ -32,7 +32,7 @@ function App() {
         </Switch>
         <Footer />
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
